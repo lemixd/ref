7b6c22255ab2a9cf301edd71865896a2f8e12c98
lemixd <lemixd@gmail.com>
2019-01-12 23:29:32 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Lottie animation properties
+
+var rocketAnim = bodymovin.loadAnimation({
+	container: document.getElementById('rocket'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'js/rocket.json'
+	});
+
+var splashAnim = bodymovin.loadAnimation({
+	container: document.getElementById('splash'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'js/splash.json'
+	});
+
+
+
+// Thousands separator for numbers
+
+function numberFormatting(numberToFormat) {
+
+	var numberToFormat = 1500483;
+
+	var parts = numberToFormat.toString().split(".");
+	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+	return parts.join(".")
+}
+
+document.getElementById('paymentCounter').innerHTML = numberFormatting();
