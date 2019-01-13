55bbafc244cc2ca93ccfd016a504c473d465f524
lemixd <lemixd@gmail.com>
2019-01-13 23:01:43 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -15,6 +15,14 @@ var splashAnim = bodymovin.loadAnimation({
 	path: 'js/splash.json'
 	});
 
+var spacegirlAnim = bodymovin.loadAnimation({
+	container: document.getElementById('spacegirl'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'js/spacegirl.json'
+	});
+
 
 // Thousands separator for numbers
 function numberFormatting(numberToFormat) {
@@ -27,3 +35,27 @@ function numberFormatting(numberToFormat) {
 }
 
 document.getElementById('paymentCounter').innerHTML = numberFormatting();
+
+
+// Parallax
+window.addEventListener('scroll', function(e) {
+
+	const target = document.querySelectorAll('.scroll');
+
+
+	var index = 0, length = target.length;
+	for (index; index < length; index++) {
+		var pos = window.pageYOffset * target[index].dataset.rate;
+
+		if(target[index].dataset.direction === 'vertical') {
+			target[index].style.transform = 'translate3d(0px,'+pos+'px, 0px)';
+		} else {
+			var posX = window.pageYOffset * target[index].dataset.ratex;
+			var posY = window.pageYOffset * target[index].dataset.ratey;
+			
+			target[index].style.transform = 'translate3d('+posX+'px, '+posY+'px, 0px)';
+		}
+	}
+
+
+});
