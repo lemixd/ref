cc9a3f8ccc3fdb5d564655d3994a89b650aeb3fa
Дмитро Галушка <d.galushka@taslife.com.ua>
2019-01-14 14:15:27 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+// Lottie animation properties
 var rocketAnim = bodymovin.loadAnimation({
 	container: document.getElementById('rocket'),
 	renderer: 'svg',
 	loop: true,
 	autoplay: true,
 	path: 'js/rocket.json'
-	})
+	});
 
 var splashAnim = bodymovin.loadAnimation({
 	container: document.getElementById('splash'),
@@ -12,4 +13,58 @@ var splashAnim = bodymovin.loadAnimation({
 	loop: true,
 	autoplay: true,
 	path: 'js/splash.json'
-	})
+	});
+
+var spacegirlAnim = bodymovin.loadAnimation({
+	container: document.getElementById('spacegirl'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'js/spacegirl.json'
+	});
+
+
+// Thousands separator for numbers
+function numberFormatting(numberToFormat) {
+
+	var numberToFormat = 1500483;
+
+	var parts = numberToFormat.toString().split(".");
+	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+	return parts.join(".")
+}
+
+document.getElementById('paymentCounter').innerHTML = numberFormatting();
+
+
+// Parallax
+window.addEventListener('scroll', function(e) {
+
+	const target = document.querySelectorAll('.scroll');
+
+	var index = 0, length = target.length;
+	for (index; index < length; index++) {
+		var pos = window.pageYOffset * target[index].dataset.rate;
+
+		if(target[index].dataset.direction === 'vertical') {
+			target[index].style.transform = 'translate3d(0px,'+pos+'px, 0px)';
+		} else {
+			var posX = window.pageYOffset * target[index].dataset.ratex;
+			var posY = window.pageYOffset * target[index].dataset.ratey;
+			
+			target[index].style.transform = 'translate3d('+posX+'px, '+posY+'px, 0px)';
+		}
+	}
+});
+
+
+
+// Video modal toggle
+function toggleVideo(state) {
+    // if state == 'hide', hide. Else: show video
+    var div = document.getElementById("popup-video");
+    var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
+    div.style.display = state == 'hide' ? 'none' : '';
+    func = state == 'hide' ? 'pauseVideo' : 'playVideo';
+    iframe.postMessage('{"event":"command","func":"' + func + '","args":""}','*');
+}
